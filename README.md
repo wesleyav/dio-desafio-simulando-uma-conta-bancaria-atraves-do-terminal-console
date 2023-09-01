3183c0f2b74bb2b0e4e4e1b7a038bc932e011ed4
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-09-01 10:25:22 -0300

    Adiciona implementação do desafio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dio-desafio-simulando-uma-conta-bancaria-atraves-do-terminal-console
 Repositório para o desafio da DIO - Simulando Uma Conta Bancária Através Do Terminal/Console
+
+Instruções do desafio [aqui.](https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe)
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * @author Wesley
+ * @version 1.0
+ * @since 01/09/2023
+ */
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Informe o numero da agencia:");
+		String agencia = scanner.nextLine();
+
+		System.out.println("Informe o numero da conta:");
+		int numeroDaConta = scanner.nextInt();
+
+		System.out.println("Informe o nome do cliente:");
+		String nomeDoCliente = scanner.nextLine();
+		scanner.nextLine();
+
+		System.out.println("Informe o saldo:");
+		Double saldo = scanner.nextDouble();
+
+		scanner.close();
+
+		System.out.println("Ola " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agencia e "
+				+ agencia + ", conta " + numeroDaConta + " e seu saldo " + saldo + " ja esta disponivel para saque.");
+	}
 }
